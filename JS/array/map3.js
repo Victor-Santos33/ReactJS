7bd465691ff1079abbe55c2b883bb88b63fbf003
Victor Lima <81842427+Victor-Santos33@users.noreply.github.com>
2023-03-17 00:20:40 -0300

    filter atualizando

--- JS/array/map3.js
+++ JS/array/map3.js
@@ -1,7 +1,7 @@
 Array.prototype.map2 = function(callback){
     const newArray = []
     for (let i = 0; i < this.length; i++){
-        newArray.push(callback(this[i], i , this))
+        newArray.push(callback(this[i], i, this))
     }
     return newArray
 }
